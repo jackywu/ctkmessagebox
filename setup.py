39ab17c48e048543ce1f19c3e57d147fad2fbc2c
Lucas Hoffman <hoffmanlucas@gmail.com>
2024-10-09 10:01:04 +1300

    setup pip

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ctkmessagebox2",  # Nome do pacote que será usado para instalar via pip
-    version="0.3.1",  # Versão inicial do pacote
+    name="ctkmessagebox2",
+    version="0.3.1",
     packages=find_packages(),  # Localiza todos os pacotes Python dentro do diretório
     include_package_data=True,  # Inclui arquivos como ícones (definidos em MANIFEST.in)
     description="The MessageBox package for CustomTkinter",
-    long_description=open('README.md').read(),  # Lê o README.md como descrição longa
-    long_description_content_type="text/markdown",  # Tipo de conteúdo do README
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author="Lucas Hoffman",
     author_email="hoffmanlucas@gmail.com",
-    url="https://github.com/hoffmanlucas/ctkmessagebox.git",  # URL do seu repositório GitHub
+    url="https://github.com/hoffmanlucas/ctkmessagebox.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",  # Versão mínima do Python
-    install_requires=[  # Dependências do pacote, se houver
+    python_requires=">=3.6",
+    install_requires=[
         "customtkinter",
         "Pillow"
     ],
